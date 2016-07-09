24af13e7258d9c4e8b8b528c5151f1e7128c88e2
Lindsay Ward <lindsay.ward@jcu.edu.au>
2016-07-09 17:44:37 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,3 +1,11 @@
 """
 Lindsay Ward
 """
+
+name = input("Name: ")
+while name == "":
+    print("Name can not be blank")
+    name = input("Name: ")
+for i in range(0, len(name), 2):
+    print(name[i])
+
