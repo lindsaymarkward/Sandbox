fa8950d2892c00aff42f4847a8aa02149af048dc
Lindsay Ward <lindsay.ward@jcu.edu.au>
2016-07-10 22:28:53 +1000

    Add file listing program

--- oddName.py
+++ oddName.py
@@ -2,10 +2,24 @@
 Lindsay Ward
 """
 
-name = input("Name: ")
-while name == "":
-    print("Name can not be blank")
-    name = input("Name: ")
-for i in range(0, len(name), 2):
-    print(name[i])
 
+def main():
+    name = get_name()
+    number = int(input("Print every nth character. n="))
+    print_characters(name, number)
+
+
+def print_characters(string, number):
+    for i in range(0, len(string), number):
+        print(string[i])
+
+
+def get_name():
+    name = input("Name: ")
+    while name == "":
+        print("Name can not be blank")
+        name = input("Name: ")
+    return name
+
+
+main()
